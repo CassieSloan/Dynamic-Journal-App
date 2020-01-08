6af16ff8ec737b0fa42818155986e6738635df77
Cassie <cassandra.sloan16@gmail.com>
2020-01-08 11:56:12 +1100

    installed react router dom and connected paths/componants

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,22 @@ import React, { Component } from "react";
 import HomePage from "./pages/HomePage";
 import CategorySelectionPage from "./pages/CategorySelectionPage";
 import NewEntryPage from "./pages/NewEntryPage";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
+
+
     render() {
         return(
-            <>
-                <HomePage/>
-                <CategorySelectionPage/>
-                <NewEntryPage/>
-            </>
+            <div>
+                <BrowserRouter>
+                    <>
+                        <Route exact path = "/" component={HomePage} />
+                        <Route exact path = "/category" component={CategorySelectionPage} />
+                        <Route exact path = "/entry" component={NewEntryPage} />
+                    </>
+                </BrowserRouter>
+            </div>
         )
     }
 }
